258c580af248c2e8b661e432132b29adb41debcc
pauljackals <paul.jackals@gmail.com>
2019-11-19 16:20:24 +0100

    updating main.py

--- project/main.py
+++ project/main.py
@@ -1,3 +1,8 @@
-import vehicles.Vehicle
+from people.Client import Client
+from vehicles.Car import Car
 
+client = Client("John", "Doe")
+car = Car("XY54343")
 
+client.set_vehicle(car)
+car.set_owner(client)
