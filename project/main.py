b3fbe683683e3b8404d98a5706be4380b4ad1223
pauljackals <paul.jackals@gmail.com>
2019-12-05 20:55:52 +0100

    clear terminal

--- project/main.py
+++ project/main.py
@@ -1,5 +1,13 @@
+import os
+import platform
+
 from people.mechanic import Mechanic
 from vehicles.car import Car
+
+if platform.system() == "Windows":
+    os.system('cls')
+elif platform.system() == "Linux":
+    os.system('clear')
 
 car = Car()
 client = car.get_owner()
