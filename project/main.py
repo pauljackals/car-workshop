1e8270a03cda38f64755bf22700a11e8c73c34d1
pauljackals <paul.jackals@gmail.com>
2019-12-05 23:11:48 +0100

    added Main class

--- project/main.py
+++ project/main.py
@@ -4,17 +4,27 @@ import platform
 from people.mechanic import Mechanic
 from vehicles.car import Car
 
-if platform.system() == "Windows":
-    os.system('cls')
-elif platform.system() == "Linux":
-    os.system('clear')
 
-car = Car()
-client = car.get_owner()
-mechanic = Mechanic()
+class Main:
 
-car.print_info()
-print()
-client.print_info()
-print()
-mechanic.print_info()
+    @staticmethod
+    def main():
+
+        if platform.system() == "Windows":
+            os.system("cls")
+        elif platform.system() == "Linux":
+            os.system("clear")
+
+        car = Car()
+        client = car.get_owner()
+        mechanic = Mechanic()
+
+        car.print_info()
+        print()
+        client.print_info()
+        print()
+        mechanic.print_info()
+
+
+if __name__ == "__main__":
+    Main.main()
